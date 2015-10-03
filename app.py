0f4ea3a18d7af52e69ea6c9e3ec6d367692bf5d4
Liutauras <lidi.uk@gmail.com>
2015-10-03 05:16:22 +0200

    Implement post and get

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-#!flask/bin/python
+#!ENV/bin/python
 from flask import Flask
+import sqlite3
+from flask import g
+import os.path
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE = 'ratings.db'
+db_path = os.path.join(BASE_DIR, DATABASE)
+
+def connect_db():
+    return sqlite3.connect(db_path)
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+@app.route('/api/v1.0/ratings/<int:guid>', methods=['GET'])
+def get_ratings(guid):
+    ratings = query_db("SELECT guid, rating, message FROM rating where guid = ?", [guid])
+    return jsonify({'rating': ratings})
+
+@app.route('/api/v1.0/ratings', methods=['POST'])
+def create_rating():
+    if not request.json or not 'guid' in request.json:
+        abort(400)
+
+    guid = request.json['guid']
+    rating = request.json['rating']
+    message = request.json['message']
+    query_db("INSERT INTO rating (guid, rating, message) VALUES (?,?,?)", [guid, rating, message])
+    g.db.commit()
+    return jsonify({'insert': "success"}), 201
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
